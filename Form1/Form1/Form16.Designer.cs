f20c3388b626dedb0c4f1f1794488ae456fd25b5
Michael Kozhanov <kozhanov312@gmail.com>
2021-03-23 15:44:54 +0300

    23.03.21

--- Form1/Form1/Form16.Designer.cs
+++ Form1/Form1/Form16.Designer.cs
@@ -50,6 +50,11 @@ namespace Form1
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.grid2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.grid22)).BeginInit();
@@ -76,7 +81,7 @@ namespace Form1
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(292, 35);
+            this.label2.Location = new System.Drawing.Point(459, 44);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(618, 27);
@@ -87,7 +92,7 @@ namespace Form1
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(292, 253);
+            this.label3.Location = new System.Drawing.Point(497, 273);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(564, 27);
@@ -98,7 +103,7 @@ namespace Form1
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(292, 501);
+            this.label4.Location = new System.Drawing.Point(553, 505);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(430, 27);
@@ -108,7 +113,7 @@ namespace Form1
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(113, 894);
+            this.button1.Location = new System.Drawing.Point(126, 881);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 41);
@@ -183,7 +188,7 @@ namespace Form1
             this.grid5.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.grid5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.grid5.ColumnHeadersVisible = false;
-            this.grid5.Location = new System.Drawing.Point(636, 553);
+            this.grid5.Location = new System.Drawing.Point(636, 536);
             this.grid5.Margin = new System.Windows.Forms.Padding(4);
             this.grid5.Name = "grid5";
             this.grid5.RowHeadersVisible = false;
@@ -210,7 +215,7 @@ namespace Form1
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(424, 143);
+            this.button2.Location = new System.Drawing.Point(126, 143);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 47);
@@ -222,7 +227,7 @@ namespace Form1
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(404, 343);
+            this.button3.Location = new System.Drawing.Point(126, 362);
             this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 43);
@@ -234,7 +239,7 @@ namespace Form1
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Location = new System.Drawing.Point(404, 553);
+            this.button4.Location = new System.Drawing.Point(126, 547);
             this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 39);
@@ -246,7 +251,7 @@ namespace Form1
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button5.Location = new System.Drawing.Point(404, 687);
+            this.button5.Location = new System.Drawing.Point(126, 686);
             this.button5.Margin = new System.Windows.Forms.Padding(4);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(100, 41);
@@ -274,7 +279,7 @@ namespace Form1
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(292, 759);
+            this.label5.Location = new System.Drawing.Point(273, 750);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(325, 27);
             this.label5.TabIndex = 18;
@@ -284,11 +289,11 @@ namespace Form1
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(277, 446);
+            this.label6.Location = new System.Drawing.Point(233, 445);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(340, 27);
+            this.label6.Size = new System.Drawing.Size(374, 27);
             this.label6.TabIndex = 19;
-            this.label6.Text = "Сортировка бинарной вставкой:";
+            this.label6.Text = "Сортировка бинарными вставками:";
             // 
             // dataGridView1
             // 
@@ -330,11 +335,65 @@ namespace Form1
             this.label7.TabIndex = 22;
             this.label7.Text = "Сортировка простым выбором:";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(292, 154);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(271, 27);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Результирующий массив:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(363, 98);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(200, 27);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Исходный массив:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(363, 327);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(200, 27);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Исходный массив:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(293, 385);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(271, 27);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "Результирующий массив:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(380, 673);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 27);
+            this.label12.TabIndex = 27;
+            // 
             // Form16
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
@@ -397,5 +456,10 @@ namespace Form1
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
     }
 }
